d925fb218b5643a4a8e45ca531cb22644cc0ed60
Daniel Fischer <fischer.daniel94@googlemail.com>
2017-06-17 17:29:56 +0200

    consolidated ObjectTools generic parameter order (Object type last)

--- src/Toolkit/ObjectTools.ts
+++ src/Toolkit/ObjectTools.ts
@@ -9,7 +9,7 @@ export type ObjMap<Obj, T> = { [name in keyof Obj]: T };
 export type ObjMapPart<Obj, T> = Partial<ObjMap<Obj, T>>;
 
 export default class ObjectTools {
-	static map<Obj, T, O>(obj: Obj, fn: (value: T, key: keyof Obj) => O): ObjMap<Obj, O> {
+	static map<T, O, Obj>(obj: Obj, fn: (value: T, key: keyof Obj) => O): ObjMap<Obj, O> {
 		const mapped = Object.entries<T, Obj>(obj).map(([key, value]: [keyof Obj, T]): ObjMapPart<Obj, O> => {
 			let result: ObjMapPart<Obj, O> = {};
 			result[key] = fn(value, key);
@@ -22,7 +22,7 @@ export default class ObjectTools {
 		return Object.assign<ObjMap<Obj, O>>({}, ...arr.map(value => fn(value)));
 	}
 
-	static forEach<Obj, T>(obj: Obj, fn: (value: T, key: keyof Obj) => void): void {
+	static forEach<T, Obj>(obj: Obj, fn: (value: T, key: keyof Obj) => void): void {
 		Object.entries(obj).forEach(([key, value]: [keyof Obj, T]) => fn(value, key));
 	}
 }
