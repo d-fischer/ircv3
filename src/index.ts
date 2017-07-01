e0daf427b7d1203a6872a6fb5dae4540676c1bf3
Daniel Fischer <fischer.daniel94@googlemail.com>
2017-07-01 16:22:40 +0200

    register core capability commands on load, expose capabilities and message base type for extending

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
-import Message from './Message/Message';
+import Message, {MessageConstructor} from './Message/Message';
 import * as MessageTypes from './Message/MessageTypes';
+import Capability from './Capability/Capability';
+import * as CoreCapabilities from './Capability/CoreCapabilities';
 import ObjectTools from './Toolkit/ObjectTools';
 
-ObjectTools.forEach(MessageTypes.Commands, (type: typeof Message) => {
+ObjectTools.forEach(MessageTypes.Commands, (type: MessageConstructor) => {
 	Message.registerType(type);
 });
 
-ObjectTools.forEach(MessageTypes.Numerics, (type: typeof Message) => {
+ObjectTools.forEach(MessageTypes.Numerics, (type: MessageConstructor) => {
 	Message.registerType(type);
+});
+
+ObjectTools.forEach(CoreCapabilities, (cap: Capability) => {
+	if (cap.messageTypes) {
+		for (const messageType of cap.messageTypes) {
+			Message.registerType(messageType);
+		}
+	}
 });
 
 export { default as Client } from './Client';
-export { MessageTypes };
+export { Message, MessageTypes };
+export { Capability, CoreCapabilities };
