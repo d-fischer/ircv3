ae824d321718b6886bbec3829323695ab463b01f
Daniel Fischer <fischer.daniel94@googlemail.com>
2017-08-19 16:30:08 +0200

    expose MessagePrefix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import Message, { MessageParam, MessageParamSpec } from './Message/Message';
+import Message, { MessageParam, MessageParamSpec, MessagePrefix } from './Message/Message';
 import * as MessageTypes from './Message/MessageTypes';
 import Capability from './Capability/Capability';
 import * as CoreCapabilities from './Capability/CoreCapabilities';
 
 export { default as Client } from './Client';
-export { Message, MessageTypes, MessageParam, MessageParamSpec };
+export { Message, MessageTypes, MessageParam, MessageParamSpec, MessagePrefix };
 export { Capability, CoreCapabilities };
