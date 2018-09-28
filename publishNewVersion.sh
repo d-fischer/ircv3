6c383cc4aee18386253e72bedf843057a46ace4d
Daniel Fischer <fischer.daniel94@gmail.com>
2018-09-28 22:21:18 +0200

    update publish script

--- publishNewVersion.sh
+++ publishNewVersion.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-pushd $(dirname $0)
-npm version ${1:-patch} -m "release version %s"
-npm publish
-popd
+CWD="$(pwd)"
+cd "$(dirname $0)"
+VERSIONTYPE="${1:-patch}"
+npm version --preid pre ${VERSIONTYPE} -m "release version %s"
+case ${VERSIONTYPE} in
+	"pre"*) npm publish --tag next ;;
+	*) npm publish ;;
+esac
+cd "$CWD"
