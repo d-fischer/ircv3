4942d844aac9c8eafd162c16cbbb74e8c6a0ce5d
Joshua Davison <ozjd@users.noreply.github.com>
2019-08-08 01:39:19 +1000

    Ensure tags: Map<string, string> is always defined

--- src/Message/MessageParser.ts
+++ src/Message/MessageParser.ts
@@ -4,97 +4,107 @@ import { ServerProperties, defaultServerProperties } from '../ServerProperties';
 import { splitWithLimit } from '../Toolkit/StringTools';
 
 export default function parseMessage(
-	line: string,
-	serverProperties: ServerProperties = defaultServerProperties,
-	knownCommands: Map<string, MessageConstructor> = coreMessageTypes,
-	isServer: boolean = false,
-	nonConformingCommands: string[] = []
+  line: string,
+  serverProperties: ServerProperties = defaultServerProperties,
+  knownCommands: Map<string, MessageConstructor> = coreMessageTypes,
+  isServer: boolean = false,
+  nonConformingCommands: string[] = [],
 ): Message {
-	const splitLine: string[] = line.split(' ');
-	let token: string;
+  const splitLine: string[] = line.split(' ');
+  let token: string;
 
-	let command: string | undefined;
-	const params: MessageParam[] = [];
-	let tags: Map<string, string> | undefined;
-	let prefix: MessagePrefix | undefined;
+  let command: string | undefined;
+  const params: MessageParam[] = [];
+  let tags: Map<string, string> = new Map();
+  let prefix: MessagePrefix | undefined;
 
-	while (splitLine.length) {
-		token = splitLine[0];
-		if (token[0] === '@' && !tags && !command) {
-			tags = parseTags(token.substr(1));
-		} else if (token[0] === ':') {
-			if (!prefix && !command) {
-				if (token.length > 1) { // Not an empty prefix
-					prefix = parsePrefix(token.substr(1));
-				}
-			} else {
-				params.push({
-					value: splitLine.join(' ').substr(1),
-					trailing: true
-				});
-				break;
-			}
-		} else if (!command) {
-			command = token.toUpperCase();
-		} else {
-			params.push({
-				value: token,
-				trailing: false
-			});
-		}
-		splitLine.shift();
-	}
+  while (splitLine.length) {
+    token = splitLine[0];
+    if (token[0] === '@' && !tags && !command) {
+      tags = parseTags(token.substr(1), tags);
+    } else if (token[0] === ':') {
+      if (!prefix && !command) {
+        if (token.length > 1) {
+          // Not an empty prefix
+          prefix = parsePrefix(token.substr(1));
+        }
+      } else {
+        params.push({
+          value: splitLine.join(' ').substr(1),
+          trailing: true,
+        });
+        break;
+      }
+    } else if (!command) {
+      command = token.toUpperCase();
+    } else {
+      params.push({
+        value: token,
+        trailing: false,
+      });
+    }
+    splitLine.shift();
+  }
 
-	if (!command) {
-		throw new Error(`line without command received: ${line}`);
-	}
+  if (!command) {
+    throw new Error(`line without command received: ${line}`);
+  }
 
-	let message: Message;
+  let message: Message;
 
-	let messageClass: MessageConstructor = Message;
-	if (knownCommands.has(command)) {
-		messageClass = knownCommands.get(command)!;
-	}
+  let messageClass: MessageConstructor = Message;
+  if (knownCommands.has(command)) {
+    messageClass = knownCommands.get(command)!;
+  }
 
-	// tslint:disable-next-line:no-inferred-empty-object-type
-	message = new messageClass(command, params, tags, prefix, serverProperties, line, isServer, !nonConformingCommands.includes(command));
+  // tslint:disable-next-line:no-inferred-empty-object-type
+  message = new messageClass(
+    command,
+    params,
+    tags,
+    prefix,
+    serverProperties,
+    line,
+    isServer,
+    !nonConformingCommands.includes(command),
+  );
 
-	return message;
+  return message;
 }
 
 export function parsePrefix(raw: string): MessagePrefix {
-	const [nick, hostName] = splitWithLimit(raw, '!', 2);
-	if (hostName) {
-		const [user, host] = splitWithLimit(hostName, '@', 2);
-		if (host) {
-			return { nick, user, host };
-		} else {
-			return { nick, host: user };
-		}
-	} else {
-		return { nick };
-	}
+  const [nick, hostName] = splitWithLimit(raw, '!', 2);
+  if (hostName) {
+    const [user, host] = splitWithLimit(hostName, '@', 2);
+    if (host) {
+      return { nick, user, host };
+    } else {
+      return { nick, host: user };
+    }
+  } else {
+    return { nick };
+  }
 }
 
 const tagUnescapeMap: { [char: string]: string } = {
-	'\\': '\\',
-	':': ';',
-	n: '\n',
-	r: '\r',
-	s: ' '
+  '\\': '\\',
+  ':': ';',
+  n: '\n',
+  r: '\r',
+  s: ' ',
 };
 
-export function parseTags(raw: string): Map<string, string> | undefined {
-	const tags: Map<string, string> = new Map();
-	const tagStrings = raw.split(';');
-	for (const tagString of tagStrings) {
-		const [tagName, tagValue] = splitWithLimit(tagString, '=', 2);
-		if (tagName === '') {
-			continue; // Ignore empty tags: @ @; @x; etc.
-		}
-		// unescape according to http://ircv3.net/specs/core/message-tags-3.2.html#escaping-values
-		tags.set(tagName, tagValue.replace(/\\([\\:nrs])/g, (_, match) => tagUnescapeMap[match]));
-	}
+export function parseTags(raw: string, map?: Map<string, string>): Map<string, string> {
+  const tags: Map<string, string> = map || new Map();
+  const tagStrings = raw.split(';');
+  for (const tagString of tagStrings) {
+    const [tagName, tagValue] = splitWithLimit(tagString, '=', 2);
+    if (tagName === '') {
+      continue; // Ignore empty tags: @ @; @x; etc.
+    }
+    // unescape according to http://ircv3.net/specs/core/message-tags-3.2.html#escaping-values
+    tags.set(tagName, tagValue.replace(/\\([\\:nrs])/g, (_, match) => tagUnescapeMap[match]));
+  }
 
-	return tags.size > 0 ? tags : undefined;
+  return tags;
 }
