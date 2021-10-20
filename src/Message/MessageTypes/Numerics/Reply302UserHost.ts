041249b0c7d9d66192ce0171162ac59974dc6261
Daniel Fischer <daniel@d-fischer.dev>
2021-10-20 14:16:13 +0200

    correct UserHost reply numeric

--- src/Message/MessageTypes/Numerics/Reply302UserHost.ts
+++ src/Message/MessageTypes/Numerics/Reply302UserHost.ts
@@ -2,7 +2,7 @@ import type { MessageParam } from '../../Message';
 import { Message } from '../../Message';
 import { MessageParamDefinition, MessageType } from '../../MessageDefinition';
 
-@MessageType('322')
+@MessageType('302')
 export class Reply302UserHost extends Message<Reply302UserHost> {
 	@MessageParamDefinition()
 	me!: MessageParam;
